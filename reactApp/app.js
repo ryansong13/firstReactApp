b8d6a231f7c95a098cc5b78f78c8032eda743d63
Ryan Song <ryansong13@gmail.com>
2018-07-26 17:10:42 -0700

    Strikes added

--- reactApp/app.js
+++ reactApp/app.js
@@ -2,7 +2,22 @@
 // var ReactDOM = require('react-dom');
 import React from 'react';
 import ReactDOM from 'react-dom'
-var dummyData = ["Clean house", "Take out trash", "Cook dinner", "Pick up brother", "Call parents"]
+var dummyData = [{
+  taskText: "Clean house",
+  completed: false
+},
+  {taskText: "Take out trash",
+  completed: false
+},
+  {taskText: "Cook dinner",
+  completed: false
+},
+  {taskText: "Pick up brother",
+  completed: false
+},
+  {taskText: "Call parents",
+  completed: false
+}]
 class TodoList extends React.Component{
   constructor(props){
     super(props)
@@ -18,14 +33,62 @@ class TodoList extends React.Component{
 
 class Todo extends React.Component{
   constructor(props){
+    super(props),
+    this.state = {
+      completed: this.props.task.completed
+    },
+    this.handleClick.bind(this)
+  }
+  handleClick(e){
+    e.preventDefault();
+    this.setState(prevState => ({
+      completed: !prevState.completed
+    }))
+  }
+  render(){
+    // function handleClick(e){
+    //   e.preventDefault();
+    //   var newstate = !this.props.task.completed;
+    //   this.setState({
+    //     completed: newstate
+    //   })
+    // }
+    if(this.state.completed === true){
+      return (<div><button onClick = {(e) => this.handleClick(e)}>X</button><strike>{this.props.task.taskText}</strike></div>)
+    }else{
+      return (<div><button onClick = {(e) => this.handleClick(e)}>X</button>{this.props.task.taskText}</div>)
+    }
+  }
+}
+
+class InputLine extends React.Component{
+  constructor(props){
     super(props)
   }
   render(){
     return(
-      <div><button>X</button>{this.props.task}</div>
+      <div><input type = 'text' placeholder = 'Todo...'/><button id = 'addtodo'>Add todo</button></div>
     )
   }
 }
 
-ReactDOM.render(<TodoList />,
+class TodoApp extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <div>
+          <InputLine />
+        </div>
+        <div>
+          <TodoList/>
+        </div>
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<TodoApp />,
    document.getElementById('root'));
